2e771483f498c9e82084140d7def4c387d41418a
Jesus <jdserrano106@gmail.com>
2022-09-05 17:19:54 -0400

    area rectangulo solucion 1

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,13 @@
+//area rectangulo solucion 1
 fn main() {
-    let cadena = String::from("hola mundo del rust");
-    println!("la primera palabra de la cadena es: {}", primera_palabra(&cadena));
-
-}
-
-fn primera_palabra(cadena: &str) -> &str {
-    let bytes = cadena.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate(){
-        if item == b' '{
-            return &cadena[0..i];
-        }
-    }
-    &cadena[..]
+    let ancho = 15;
+    let alto = 24;
+    println!("el area del rectangulo es: {}", area(ancho, alto));
 }
 
 
-
+fn area(ancho: u32, alto: u32) -> u32 {
+    ancho * alto
+}
 
 
