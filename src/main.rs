511e551ae6e7b9eda8a7958c76766ece843b2d4c
Jesus <jdserrano106@gmail.com>
2022-09-05 17:24:10 -0400

    area rectangulo solucion 2

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-//area rectangulo solucion 1
+//area rectangulo solucion 2
 fn main() {
-    let ancho = 15;
-    let alto = 24;
-    println!("el area del rectangulo es: {}", area(ancho, alto));
+    let rect1 = (15, 24);
+    println!("el area del rectangulo es: {}", area(rect1));
 }
 
 
-fn area(ancho: u32, alto: u32) -> u32 {
-    ancho * alto
+fn area(rectangulo: (u32, u32)) -> u32 {
+    rectangulo.0 * rectangulo.1
 }
 
 
